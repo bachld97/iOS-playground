8e4549c8c8d2f7cef87787b3c7ac8cc4638028f4
Bach Le <bachld97@gmail.com>
2020-03-19 21:56:15 +0700

    Go to home page when click on avatar

--- frontend/bblog/src/shared/PersistentHeader.js
+++ frontend/bblog/src/shared/PersistentHeader.js
@@ -4,20 +4,23 @@ import '../App.css';
 
 
 class PersistentHeader extends React.Component {
-    render() {
-      return (
-        <div className='App-header'>
-          <img src={myProfilePic} 
-            className='Header-Avatar' 
-            alt='Avatar' 
-          />
-          <h1> 
-            Le Duy Bach
-          </h1>
-          <a href="https://medium.com/@ldbach97">Click to visit me on Medium!!</a>
-        </div>
-      );
-    }
+  onAvatarClick() {
+
   }
 
-  export default PersistentHeader;
+  render() {
+    return (
+      <div className='App-header'>
+        <a href="/">
+          <img src={myProfilePic} className='Header-Avatar' alt='Avatar'/>
+        </a>
+        <h1> 
+          Le Duy Bach
+        </h1>
+        <a href="https://medium.com/@ldbach97">Click to visit me on Medium!!</a>
+      </div>
+    );
+  }
+}
+
+export default PersistentHeader;
